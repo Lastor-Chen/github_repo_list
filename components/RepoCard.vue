f39a1147198cf675b7d108e4ed768106581d989f
Lastor <lastoraya@gmail.com>
2020-03-17 23:11:25 +0800

    [加入] RepoCard 串接 API

--- components/RepoCard.vue
+++ components/RepoCard.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <a href="#">
-        <h5 class="card-title d-inline-block">Repo name</h5>
+      <a :href="repo.html_url" target="_blank" rel="noopener">
+        <h5 class="card-title d-inline-block">{{repo.name}}</h5>
       </a>
-      <p class="card-text">des</p>
+      <div class="card-subtitle">{{repo.created_at}}</div>
+      <p class="card-text">{{repo.description || 'No description...'}}</p>
     </div>
   </div>
 </template>
+
+<script>
+module.exports = {
+  props: {
+    repo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- main.js
+++ main.js
@@ -8,5 +8,31 @@ new Vue({
   components: {
     UserCard,
     RepoCard
+  },
+  data() {
+    return {
+      repos: []
+    }
+  },
+  created() {
+    this.fetchRepos()
+  },
+  methods: {
+    async fetchRepos() {
+      try {
+        const { data } = await axios.get('https://api.github.com/users/lastor-chen/repos?sort=created')
+
+        this.repos = data.map(repo => ({
+          id: repo.id,
+          name: repo.name,
+          description: repo.description,
+          html_url: repo.html_url,
+          created_at: repo.created_at
+        }))
+
+      } catch (err) {
+        console.log(err)
+      }
+    }
   }
 })
