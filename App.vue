f1cc132714285db0db8ea52d51d2f32e1a3261a4
Lastor <lastoraya@gmail.com>
2020-03-17 23:17:16 +0800

    [重構] 拆分出 App.vue，統一結構

---
+++ App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container pt-5">
+    <div class="row">
+      <aside class="col-md-3">
+        <user-card/>
+      </aside>
+      <div class="col-md-9">
+        <span class="font-weight-bold">Repositories</span>
+        <span>{{repos.length}}</span>
+        <hr>
+        <main role="main">
+          <repo-card
+            class="mb-3"
+            v-for="repo in repos"
+            :key="repo.id"
+            :repo="repo"
+          />
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const UserCard = httpVueLoader('./components/UserCard.vue')
+const RepoCard = httpVueLoader('./components/RepoCard.vue')
+
+module.exports = {
+  components: {
+    UserCard,
+    RepoCard
+  },
+  data() {
+    return {
+      repos: []
+    }
+  },
+  created() {
+    this.fetchRepos()
+  },
+  methods: {
+    async fetchRepos() {
+      try {
+        const { data } = await axios.get('https://api.github.com/users/lastor-chen/repos?sort=created')
+
+        this.repos = data.map(repo => ({
+          id: repo.id,
+          name: repo.name,
+          description: repo.description,
+          html_url: repo.html_url,
+          created_at: repo.created_at
+        }))
+
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
--- main.js
+++ main.js
@@ -1,38 +1,10 @@
 // title (標題)、description (專案描述)、url (專案網址)，其餘資訊自行決定
 
-const UserCard = httpVueLoader('./components/UserCard.vue')
-const RepoCard = httpVueLoader('./components/RepoCard.vue')
+const App = httpVueLoader('./App.vue')
 
 new Vue({
   el: '#app',
   components: {
-    UserCard,
-    RepoCard
-  },
-  data() {
-    return {
-      repos: []
-    }
-  },
-  created() {
-    this.fetchRepos()
-  },
-  methods: {
-    async fetchRepos() {
-      try {
-        const { data } = await axios.get('https://api.github.com/users/lastor-chen/repos?sort=created')
-
-        this.repos = data.map(repo => ({
-          id: repo.id,
-          name: repo.name,
-          description: repo.description,
-          html_url: repo.html_url,
-          created_at: repo.created_at
-        }))
-
-      } catch (err) {
-        console.log(err)
-      }
-    }
+    App
   }
 })
